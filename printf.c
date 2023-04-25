0bf94f9fd7d0279d1f122fd349033588d27002dd
phillmashie <phillmashie@gmail.com>
2023-04-25 18:47:41 +0000

    printf

--- printf.c
+++ printf.c
@@ -9,21 +9,21 @@
  */
 int _printf(const char *format, ...)
 {
-	/* declaring variables */
-
-	/* looping through the formatted string */
-
-		/* condition for when the loop encounters the % sign */
-		
-		/* if % sign is encountered, check the character next to it (if s[i] == %, check s[i] + 1 */
-
-		/* check if the next character is either c, s, or % */
-
-		/* Switch or if statement */
-
-		/* s: s = va_arg(list, char *) */
-
-		/*c: c = (char) va_arg(list, int) */
-
-		/*%: as itself */
+	int i, printed = 0, printed_chars = 0;
+	if(fomart == NULL)
+		return(-1);
+	for(i=0;format&&format[i] != '\0';i++)
+	{
+		printed_chars++;
+	}
+	return(printed_chars);
 }
+/* declaring variables */
+/* looping through the formatted string */
+/* condition for when the loop encounters the % sign */
+/* if % sign is encountered, check the character next to it (if s[i] == %, check s[i] + 1 */
+/* check if the next character is either c, s, or % */
+/* Switch or if statement */
+/* s: s = va_arg(list, char *) */
+/*c: c = (char) va_arg(list, int) */
+/*%: as itself */
