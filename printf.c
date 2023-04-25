039becbf78c0a963700dd31a8ef7b9b5b6bbd999
pitsomashilo <mmapitsomashilo@gmail.com>
2023-04-25 18:28:39 +0000

    Added pseudocode to printf.c

---
+++ printf.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdarg.c>
+
+/**
+ * _printf - prints to stdout and returns number of characters printed
+ * @format: formatted string
+ *
+ * Return: integer - number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	/* declaring variables */
+
+	/* looping through the formatted string */
+
+		/* condition for when the loop encounters the % sign */
+		
+		/* if % sign is encountered, check the character next to it (if s[i] == %, check s[i] + 1 */
+
+		/* check if the next character is either c, s, or % */
+
+		/* Switch or if statement */
+
+		/* s: s = va_arg(list, char *) */
+
+		/*c: c = (char) va_arg(list, int) */
+
+		/*%: as itself */
+}
