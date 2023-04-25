2a421985f2e643527e17f96bc7e99e457bf80b9e
phillmashie <phillmashie@gmail.com>
2023-04-25 19:00:28 +0000

    printf

--- printf.c
+++ printf.c
@@ -10,18 +10,19 @@
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
-	if(fomart == NULL)
-		return(-1);
-	for(i=0;format&&format[i] != '\0';i++)
+
+	if (fomart == NULL)
+		return (-1);
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		printed_chars++;
 	}
-	return(printed_chars);
+	return (printed_chars);
 }
 /* declaring variables */
 /* looping through the formatted string */
 /* condition for when the loop encounters the % sign */
-/* if % sign is encountered, check the character next to it (if s[i] == %, check s[i] + 1 */
+/* if % sign is enco, check the character next to it (if s[i] == %, check s[i] + 1 */
 /* check if the next character is either c, s, or % */
 /* Switch or if statement */
 /* s: s = va_arg(list, char *) */
