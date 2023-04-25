c36df59694a103eb72e6caf9c9531782986ddc2c
pitsomashilo <mmapitsomashilo@gmail.com>
2023-04-25 20:13:26 +0000

    Made the following changes:
    => Took the switch statement that checks for the data type and turned it
    into a function
    => Added function prototypes at the top of the file

--- printf.c
+++ printf.c
@@ -7,11 +7,13 @@
  *
  * Return: integer - number of characters printed
  */
+int _printf(const char *format, ...);
+void convert(char x);
+
 int _printf(const char *format, ...)
 {
-	int i, j, printed = 0, printed_chars = 0;
-	char c, x;
-	char *s;
+	int i, printed = 0, printed_chars = 0;
+	char x;
 	va_list list;
 
 	va_start(list, format);
@@ -21,35 +23,47 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%' || format[i] != 'c' || format[i] != 'c')
 		{
+			_putchar(format[i]);
 			printed_chars++;
 		}
 		else if (format[i] == '%')
 		{
 			x = format[i + 1];
-			switch (x)
-				case 'c':
-					c = (char) va_arg(list, int);
-					_putchar(c);
-					printed_chars++;
-					break;
-				case 's':
-					s = va_arg(list, char *);
-					for (j = 0; s[j] != '\0'; j++)
-					{
-						_putchar(s[j]);
-						printed_chars++;
-					}
-				break;
-				case '%'
-					_putchar('%');
-					printed_chars++;
-					break;
+			/* replaced the block of code that was here with the function convert */
+			convert(x);
 		}
 		else
 		{
 			printed = handle_print(format);
 		}
 	}
-	va_end(list);
 	return (printed_chars);
+}
+/**
+ * convert - converts format specifier to correct output
+ * @x: char
+ */
+void convert(char x)
+{
+	int j; /* counter variable */
+	char c, *s;
+
+	switch (x)
+		case 'c':
+			c = (char) va_arg(list, int);
+			_putchar(c);
+			printed_chars++;
+			break;
+		case 's':
+			s = va_arg(list, char *);
+			for (j = 0; s[j] != '\0'; j++)
+			{
+				_putchar(s[j]);
+				printed_chars++;
+			}
+			break;
+		case '%':
+			_putchar('%');
+			printed_chars++;
+			break;
 }
