9cf81e7cc82a38afd45e8d1d3e83f2c04d87b7b5
pitsomashilo <mmapitsomashilo@gmail.com>
2023-04-25 19:44:32 +0000

    Added an if statement to identify % sign and a switch statement to use format specifier to print the correct output

--- printf.c
+++ printf.c
@@ -9,15 +9,39 @@
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
+	int i, j, printed = 0, printed_chars = 0;
+	char c, x;
+	char *s;
 
-	if (fomart == NULL)
+	if (format == NULL)
 		return (-1);
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (fomart[i] != '%' || format[i] != 'c' || fomart[i] != 'c')
+		if (format[i] != '%' || format[i] != 'c' || format[i] != 'c')
 		{
 			printed_chars++;
+		}
+		else if (format[i] == '%')
+		{
+			x = format[i + 1];
+			switch (x)
+				case 'c':
+					c = (char) va_arg(list, int);
+					_putchar(c);
+					printed_chars++;
+					break;
+				case 's':
+					s = va_arg(list, char *);
+					for (j = 0; s[j] != '\0'; j++)
+					{
+						_putchar(s[j]);
+						printed_chars++;
+					}
+				break;
+				case '%'
+					_putchar('%');
+					printed_chars++;
+					break;
 		}
 		else
 		{
